6a5e0f9a1b8ccacc3d983a395075f8653a183f1a
fengge <fengge1230611@com.gmail>
2025-12-24 17:30:09 +0800

    更新控制层代码和配置文件端口

---
+++ admin/src/main/java/org/dami/shortlink/admin/controller/UserController.java
@@ -0,0 +1,17 @@
+package org.dami.shortlink.admin.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ */
+@RestController
+public class UserController {
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public String getUserByUsername(@PathVariable String username){
+        return "Hi " + username;
+    }
+}
